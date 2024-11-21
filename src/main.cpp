0d446d51bf0bbdd7c6c369dcbafac5f2ec6946fb
Spencer Graffunder <spencergraffunder@cedarville.edu>
2024-11-21 18:47:52 -0500

    clean code

--- src/main.cpp
+++ src/main.cpp
@@ -19,34 +19,20 @@
 // The SPIFFS (FLASH filing system) is used to hold touch screen
 // calibration data
 
-#include "haltech_can.h"
 #include "FS.h"
 #include <SPI.h>
-#include <TFT_eSPI.h>      // Hardware-specific library
-#include "screen.h"
-#include "main.h"
-
-
-HaltechCan htc;
+#include <CAN.h>
 
 void setup() {
   // Use serial port
   Serial.begin(115200);
-  delay(2000);
-
+  delay(500);
   Serial.println("setup");
+  delay(500);
 
-  //screenSetup();
-
-  if (!htc.begin(1000E3)) {
-    while (1) {
-      Serial.println("Haltech CAN init failed");
-      delay(1000);
-    }
-  }
+  
 }
 
 void loop(void) {
-  //screenLoop();
-  htc.process();
+  
 }
