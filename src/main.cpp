1f66bfce6d15b4266a98f5eba16ec9dfec0bebb4
Spencer Graffunder <spencergraffunder@cedarville.edu>
2024-11-26 15:59:18 -0500

    small updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,4 @@
 #include <Arduino.h>
-
-/*
-  The TFT_eSPI library incorporates an Adafruit_GFX compatible
-  button handling class, this sketch is based on the Arduin-o-phone
-  example.
-
-  This example displays a keypad where numbers can be entered and
-  sent to the Serial Monitor window.
-
-  The sketch has been tested on the ESP8266 (which supports SPIFFS)
-
-  The minimum screen size is 320 x 240 as that is the keypad size.
-
-  TOUCH_CS and SPI_TOUCH_FREQUENCY must be defined in the User_Setup.h file
-  for the touch functions to do anything.
-*/
-
-// The SPIFFS (FLASH filing system) is used to hold touch screen
-// calibration data
-
 #include "haltech_can.h"
 #include "FS.h"
 #include <SPI.h>
